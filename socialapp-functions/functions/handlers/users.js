593dc08aab6669c9df2c69a2789256c3e2fe2071
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-07-17 12:57:07 +0100

    upload image

--- socialapp-functions/functions/handlers/users.js
+++ socialapp-functions/functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require("../util/admin");
+const { db , admin } = require("../util/admin");
 
 const config = require("../util/config");
 
@@ -24,6 +24,7 @@ exports.signup = (req, res) => {
 
   if (!valid) return res.status(400).json({ errors });
 
+  const noImge = 'no-img.png';
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
@@ -46,6 +47,7 @@ exports.signup = (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl : `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImge}?alt=media` ,
         userId
       };
       return db
@@ -95,3 +97,53 @@ exports.login = (req, res) => {
       } else return res.status(500).json({ error: error.code });
     });
 };
+
+
+exports.uploadImage = (req,res) =>{
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs= require('fs');
+
+  const busboy = new BusBoy({headers : req.headers});
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+  busboy.on('file',(fieldname, file, filename, encoding, mimetype)=>{
+
+    console.log(fieldname,filename,mimetype);
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+     imageFileName = `${new Date().getMilliseconds}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(),imageFileName);
+    imageToBeUploaded = {filepath,mimetype}
+
+    file.pipe(fs.createWriteStream(filepath));
+
+  });
+  busboy.on('finish',()=>{
+
+    admin.storage().bucket().upload(imageToBeUploaded.filepath,{
+      resumable : false,      
+      metadata : {
+        metadata : {
+          contentType : imageToBeUploaded.mimetype
+        }
+      }
+    }).then(()=>{
+      const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media` 
+    
+      return db.collection('users').doc(req.user.handle).update({imageUrl});
+    
+    })
+    .then(()=>{
+      return res.json({message : "message uploaded successfully"})
+    })
+    .catch(error=>{
+      console.log(error);
+      return res.status(500).json({error : error.code})
+    })
+  })
+
+  busboy.end(req.rawBody);
+  
+}
--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 
 const app = require("express")();
 
-const {signup,login} = require('./handlers/users');
+const {signup,login,uploadImage} = require('./handlers/users');
 const {getAllScreams,postOneScream} = require('./handlers/screams');
 
 const FBAuth = require('./util/fbAuth');
@@ -16,6 +16,7 @@ app.post('/login', login)
 //Scream route
 app.get("/screams",getAllScreams);
 app.post("/scream",FBAuth, postOneScream);
+app.post('/user/image',FBAuth,uploadImage)
 
 
 
