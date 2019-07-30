93f39594359d7265a64ed6722959c3f693867788
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-07-30 09:25:55 +0100

    AuthRoute

--- social-app-client/src/App.js
+++ social-app-client/src/App.js
@@ -3,17 +3,36 @@ import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import themeObject from './util/theme'
-
+import jwtDecode from 'jwt-decode';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+//Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signUp";
-import Navbar from "./components/Navbar";
 
+//Compoenentt
+import Navbar from "./components/Navbar";
+import AuthRoute from './util/AuthRoute'
 
 const theme = createMuiTheme(themeObject)
+
+let authenticated;
+const token  = localStorage.FBIdToken;
+if(token){
+
+  const decodeToken  = jwtDecode(token);
+  console.log(decodeToken)
+  if(decodeToken.exp * 1000 < Date.now()){
+    window.location.href = '/login';
+    authenticated = false;
+  }else{
+    authenticated= true;
+  }
+}
+
+
 class App extends Component  {
 
   render(){
@@ -25,8 +44,8 @@ class App extends Component  {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
+              <AuthRoute  path="/login" component={login} authenticated={authenticated} />
+              <AuthRoute  path="/signup" component={signup}  authenticated={authenticated}/>
             </Switch>
           </div>
         </Router>
