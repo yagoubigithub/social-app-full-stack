94ae3e2bc1447a46f19b2cb4e85c3ea9cfb747d1
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-09-08 20:34:38 +0100

    add notifications listener

--- social-app-client/src/components/layout/Notifications.js
+++ social-app-client/src/components/layout/Notifications.js
@@ -21,100 +21,151 @@ import ChatIcon from "@material-ui/icons/Chat";
 import { connect } from "react-redux";
 import { markNotificationsRead } from "../../redux/actions/userActions";
 
+//firebase
+import db from "../../config/fbConfig";
+
 class Notifications extends Component {
   state = {
-    anchorEl: null
+    anchorEl: null,
+    notifications: []
   };
-  handleOpen = event =>{
-      this.setState({
-          anchorEl : event.target
-      })
-  }
-  handleClose = () =>{
+  handleOpen = event => {
+    this.setState({
+      anchorEl: event.target
+    });
+  };
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  onMenuOpened = () => {
+    let unreadnotifcatinIds = this.props.notifications
+      .filter(not => not.read === false)
+      .map(not => not.notificationId);
+    this.props.markNotificationsRead(unreadnotifcatinIds);
+  };
 
-    this.setState({anchorEl : null})
-  }
-  onMenuOpened=()=>{
-     let unreadnotifcatinIds = this.props.notifications
-     .filter(not=>not.read === false)
-     .map(not=> not.notificationId)
-     this.props.markNotificationsRead(unreadnotifcatinIds);
-  }
+  componentWillReceiveProps = nextProps => {
+    this.setState({ notifications: nextProps.user.notifications });
+    db.collection("notifications")
+      .where("recipient", "==", nextProps.user.credentials.handle)
+      .where("read", "==", false)
+      .orderBy("createdAt", "desc")
+      .onSnapshot(
+        snapshot => {
+          const notifications = [...this.state.notifications];
+    
+          snapshot.docChanges().forEach(change=>{
+            if (change.type === "added") {
+              if(notifications.length > 0){
+                const newNotDate = new Date(change.doc.data().createdAt);
+                const oldNotDate = new Date(notifications[0].createdAt);
+                if (newNotDate > oldNotDate) {
+                  notifications.unshift(change.doc.data());
+                }
+                console.log(change.doc.data());
+              }else{
+               
+                  notifications.unshift(change.doc.data());
+                
+              }
+             
+          }
+          });
+          this.setState({ notifications });
+          console.log(this.state.notifications);
+          /*
+          snapshot.docChanges(change => {
+            if (change.type === "added") {
+              snapshot.forEach(doc => {
+                const newNotDate = new Date(doc.data().createdAt);
+                const oldNotDate = new Date(notifications[0].createdAt);
+                if (newNotDate > oldNotDate) {
+                  notifications.unshift(doc.data());
+                }
+                console.log(doc.data());
+              });
+              this.setState({ notifications });
+              console.log(this.state.notifications);
+            }
+          });*/
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  };
   render() {
-    const notifications = this.props.notifications;
+    const notifications = this.state.notifications;
     const anchorEl = this.state.anchorEl;
-    let notificationIcon =  <NotificationsIcon />
+    let notificationIcon = <NotificationsIcon />;
     dayjs.extend(relativeTime);
 
     if (notifications && notifications.length > 0) {
-      notifications.filter(not => not.read === false).length > 0 ? (
-        (notificationIcon = (
-          <Badge
-            badgeContent={
-              notifications.filter(not => not.read === false).length
-            }
-            color="secondary"
-          >
-            <NotificationsIcon />
-          </Badge>
-        ))
-      ) : (
-        notificationIcon =  <NotificationsIcon />
-      );
-    }else{
-        notificationIcon =  <NotificationsIcon />
+      notifications.filter(not => not.read === false).length > 0
+        ? (notificationIcon = (
+            <Badge
+              badgeContent={
+                notifications.filter(not => not.read === false).length
+              }
+              color="secondary"
+            >
+              <NotificationsIcon />
+            </Badge>
+          ))
+        : (notificationIcon = <NotificationsIcon />);
+    } else {
+      notificationIcon = <NotificationsIcon />;
     }
-    let notificationsMarkup = notifications && notifications.length > 0
-    ?
-    (
-        notifications.map(not=>{
-            const verb  = not.type === 'like' ? 'liked' : 'commented on';
-            const time = dayjs(not.createdAt).fromNow();
-            const iconColor = not.read ? 'primary' : 'secondary';
-            const icon = not.type === 'like'? 
-            <FavoriteIcon color={iconColor} style={{marginLeft : 10}} />
-            : <ChatIcon color={iconColor} style={{marginLeft : 10}} />;
-            return (
-                <MenuItem key={not.createdAt} onClick={this.handleClose}>
-                {icon}
-                <Typography
+    let notificationsMarkup =
+      notifications && notifications.length > 0 ? (
+        notifications.map(not => {
+          const verb = not.type === "like" ? "liked" : "commented on";
+          const time = dayjs(not.createdAt).fromNow();
+          const iconColor = not.read ? "primary" : "secondary";
+          const icon =
+            not.type === "like" ? (
+              <FavoriteIcon color={iconColor} style={{ marginLeft: 10 }} />
+            ) : (
+              <ChatIcon color={iconColor} style={{ marginLeft: 10 }} />
+            );
+          return (
+            <MenuItem key={not.createdAt} onClick={this.handleClose}>
+              {icon}
+              <Typography
                 component={Link}
                 variant="body1"
                 to={`/users/${not.recipient}/scream/${not.screamId}`}
-                >
+              >
                 {not.sender} {verb} your scream {time}
-
-                </Typography>
-
-                </MenuItem>
-            )
-
+              </Typography>
+            </MenuItem>
+          );
         })
-    )
-    :
-    (<MenuItem onClick={this.handleClose}>
-    you have no notifications yat
-
-    </MenuItem>)
+      ) : (
+        <MenuItem onClick={this.handleClose}>
+          you have no notifications yat
+        </MenuItem>
+      );
     return (
-        <Fragment>
-            <Tooltip placeholder="top" title="Notifications">
-            <IconButton aria-owns={anchorEl ? 'simple-menu': undefined}
+      <Fragment>
+        <Tooltip placeholder="top" title="Notifications">
+          <IconButton
+            aria-owns={anchorEl ? "simple-menu" : undefined}
             aria-haspopup="true"
-            onClick={this.handleOpen}>
+            onClick={this.handleOpen}
+          >
             {notificationIcon}
-            </IconButton>
-            </Tooltip>
-            <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={this.handleClose}
-            onEntered={this.onMenuOpened}
-            >
-            {notificationsMarkup}
-
-            </Menu>
-        </Fragment>
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+          onEntered={this.onMenuOpened}
+        >
+          {notificationsMarkup}
+        </Menu>
+      </Fragment>
     );
   }
 }
@@ -123,10 +174,13 @@ Notifications.propTypes = {
   notifications: PropTypes.array.isRequired
 };
 const mapActionsToProps = dispatch => ({
-    markNotificationsRead : (notificationIds)=>dispatch(markNotificationsRead(notificationIds))
+  markNotificationsRead: notificationIds =>
+    dispatch(markNotificationsRead(notificationIds))
 });
 const mapStateToProps = state => ({
-  notifications: state.user.notifications
+  notifications: state.user.notifications,
+  handle: state.user.credentials.handle,
+  user: state.user
 });
 export default connect(
   mapStateToProps,
